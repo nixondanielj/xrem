a960197d0a28a036c35bef06006f5cb0e11671da
Dan <nixon.daniel.j@gmail.com>
2014-04-12 16:44:31 -0400

    re-added gae project

---
+++ src/com/xrem/XremServlet.java
@@ -0,0 +1,11 @@
+package com.xrem;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class XremServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
