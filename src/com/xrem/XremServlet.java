f2811c330d9bac9ec5db875949da02790f1b819b
Dan <nixon.daniel.j@gmail.com>
2014-04-12 18:30:26 -0400

    reset again

--- src/com/xrem/XremServlet.java
+++ src/com/xrem/XremServlet.java
@@ -1,10 +1,12 @@
 package com.xrem;
+
 import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class XremServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 	}
